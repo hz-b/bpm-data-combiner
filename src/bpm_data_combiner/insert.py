6b9165b2073284273c8cf7eeb013ac8da85ebb92
OPerator Interface for PTB <opip@opic23cp.mlscs.bessy.de>
2024-07-19 11:20:14 +0200

    [FIX] getting data to waveform

--- src/bpm_data_combiner/insert.py
+++ src/bpm_data_combiner/insert.py
@@ -1,11 +1,14 @@
 """
 
 Todo:
-    reimplement it for 
+    reimplement it for
 """
 from typing import Sequence, Tuple
 import sys
 import numpy as np
+
+
+import pydev
 
 
 def write_rearranged_data(stream, data, name):
@@ -15,21 +18,26 @@ def write_rearranged_data(stream, data, name):
     stream.write(f"end   {name:10s} ----------------------------\n")
     stream.flush()
 
-    
+
 def insert(*, data: Sequence, x: Tuple[int, int], y: Tuple[int, int], scale: float, active: bool, index=int, verbose=False) -> Sequence:
     """
     """
+    if len(data) == 0:
+        if verbose:
+            print("Data was of length 0. I asssume start up is running")
+        return
+
     data = np.array(data)
     rearranged = data.reshape(8, -1)
     if verbose:
         print(f"inserting at index {index}")
         write_rearranged_data(sys.stdout, rearranged, "input")
-        
+
     x_val, x_std = x
     y_val, y_std = y
     del x, y
 
-    # ensure that data is gone ... 
+    # ensure that data is gone ...
     rearranged[:, index] = 0
 
     rearranged[0, index] = x_val
@@ -38,11 +46,10 @@ def insert(*, data: Sequence, x: Tuple[int, int], y: Tuple[int, int], scale: flo
     rearranged[7, index] = y_std * scale
 
     rearranged[4, index] = active * 2
-    # ndata[2, index] = y_val
-    # ndata[3, index] = y_std
 
-    ndata = rearranged.ravel()
     if verbose:
         write_rearranged_data(sys.stdout, rearranged, "output")
-    return ndata
 
+    ndata = rearranged.ravel().tolist()
+    pydev.iointr('scaled-bpm-data', ndata)
+    return ndata
